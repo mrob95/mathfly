a422dda79d802a0676b62af036b7c56d33e97bdf
Mike Roberts <42875462+mrob95@users.noreply.github.com>
2019-01-06 21:59:33 +0000

    more

--- lib/ctrl/nexus.py
+++ lib/ctrl/nexus.py
@@ -1,12 +1,8 @@
 from dragonfly.grammar.grammar_base import Grammar
 from dragonfly.grammar.recobs import RecognitionHistory
 
-# from lib import settings
-# from lib.ctrl.dependencies import DependencyMan
-from lib.ctrl.wsrdf import TimerForWSR, RecognitionHistoryForWSR
 from lib.dfplus.communication import Communicator
 from lib.dfplus.merge.ccrmerger import CCRMerger
-# from lib.dfplus.state.stack import CasterState
 
 
 class Nexus:
@@ -19,17 +15,12 @@ class Nexus:
         self.history = RecognitionHistory(20)
         self.history.register()
 
-        # self.state.set_stack_history(self.history)
         self.preserved = None
 
-        self.timer = TimerForWSR(0.025)
-        
         from dragonfly.timer import _Timer
         self.timer = _Timer(0.025)
 
         self.comm = Communicator()
-
-        # self.dep = DependencyMan()
 
         self.macros_grammar = Grammar("recorded_macros")
 
