c8e61ef4be28d27b8d3d7d978e086a25242e6d6d
Mike Roberts <42875462+mrob95@users.noreply.github.com>
2019-06-24 15:07:45 +0100

    Add imports file

---
+++ mathfly/imports.py
@@ -0,0 +1,12 @@
+from dragonfly import Dictation, MappingRule, Choice, Function, ContextAction, Repetition, Compound
+from dragonfly import Repeat, Playback, Mimic, Window, Clipboard
+
+from mathfly.lib.actions import Text, Key, Mouse, AppContext
+from mathfly.lib.integers import IntegerRefMF
+
+from mathfly.lib import control, utilities, execution, navigation
+from mathfly.lib.latex import tex_funcs
+
+from mathfly.lib.merge.mergerule import MergeRule
+from mathfly.lib.merge.nestedrule import NestedRule
+from mathfly.lib.merge.selfmodrule import SelfModifyingRule
