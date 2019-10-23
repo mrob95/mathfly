2c971c777430f4bd96f9b1cf9eeb62583dfbdfaf
Mike Roberts <mike.roberts.2k10@googlemail.com>
2019-10-23 15:32:43 +0100

    Tidying

--- _mathfly_main.py
+++ _mathfly_main.py
@@ -1,45 +1,35 @@
-from dragonfly import (Function, Grammar, Text, Dictation, Choice, Pause)
-
-import os, sys
 import logging
 import natlink
 logging.basicConfig()
 
-from mathfly.lib import control, utilities
-from mathfly.lib.merge.mergerule import MergeRule
-from mathfly.lib.merge.mergepair import MergeInf
+from mathfly.imports import *
 
 BASE_PATH = os.path.realpath(__file__).split("\\_mathfly_main.py")[0].replace("\\", "/")
 sys.path.append(BASE_PATH)
 
 CORE = utilities.load_toml_relative("config/core.toml")
+SETTINGS = utilities.load_toml_relative("config/settings.toml")
 
-MF_NEXUS = control.nexus()
-MF_NEXUS.build(True)
+Breathe.add_global_extras(
+    Dictation("text", default=""),
+    IntegerRef("n", 1, 20, 1)
+)
 
-class MainRule(MergeRule):
+Breathe.load_modules(
+    {
+        "mathfly": SETTINGS["modules"]
+    }
+)
 
+Breathe.add_commands(
+    None,
 	mapping = {
         "configure math fly [settings]":
             Function(utilities.load_config, config_name="settings.toml"),
 
-        "<enable> <name>": Function(MF_NEXUS.rule_changer),
-
         "reboot dragon": Function(utilities.reboot),
 
-        "rebuild math fly": Function(MF_NEXUS.build),
-
         "math fly help": Function(utilities.help),
-	}
-	extras=[
-		MF_NEXUS.generate_ccr_choices("name"),
-        Choice("enable", {
-            "enable": True,
-            "disable": False
-        }),
-	]
-
-grammar = Grammar('general')
-main_rule = MainRule()
-grammar.add_rule(main_rule)
-grammar.load()
+	},
+    ccr=False
+)
--- mathfly/imports.py
+++ mathfly/imports.py
@@ -11,13 +11,9 @@ from breathe import Breathe, CommandContext, CommandsRef, Exec
 from mathfly.lib.actions import Text, Key, Mouse, AppContext
 from mathfly.lib.integers import IntegerRefMF
 
-from mathfly.lib import control, utilities, execution, navigation
+from mathfly.lib import utilities, execution, navigation
 from mathfly.lib.latex import tex_funcs
 from mathfly.lib.execution import Alternating
-
-from mathfly.lib.merge.mergerule import MergeRule
-from mathfly.lib.merge.nestedrule import NestedRule
-from mathfly.lib.merge.selfmodrule import SelfModifyingRule
 
 import re, os, datetime, sys
 from subprocess import Popen
