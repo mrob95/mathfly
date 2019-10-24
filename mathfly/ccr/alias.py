0295addeb23bbc6788301af43a42ec3f1a9d600e
Mike Roberts <mike.roberts.2k10@googlemail.com>
2019-10-24 15:56:08 +0100

    Load aliases from file

--- mathfly/ccr/alias.py
+++ mathfly/ccr/alias.py
@@ -2,19 +2,30 @@ from mathfly.imports import *
 
 # aliases = utilities.load_toml_relative("config/aliases.toml")
 
-aliases = DictList("aliases")
+aliases = DictList("aliases", utilities.load_toml_relative("config/aliases.toml"))
 
 def add_alias(spec):
     _, text = utilities.read_selected(True)
     if text and spec:
         aliases[spec] = text
+        utilities.save_toml_relative(aliases, "config/aliases.toml")
+
+def delete_aliases():
+    aliases.clear()
+    utilities.save_toml_relative(aliases, "config/aliases.toml")
+
+def delete_alias(alias):
+    del aliases[alias]
+    utilities.save_toml_relative(aliases, "config/aliases.toml")
 
 Breathe.add_commands(
     None,
     {
         "alias <text>": lambda text: add_alias(text),
-        "delete aliases": lambda: aliases.clear()
+        "delete all aliases": delete_aliases,
+        "delete alias <alias>": delete_alias,
     },
+    [DictListRef("alias", aliases),],
     ccr=False
 )
 
